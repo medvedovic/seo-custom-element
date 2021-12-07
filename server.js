ef7a1bf962aa6918bc99ccf5ec5c87ea47d7848f
Patrik Medveď <patrikm@kentico.com>
2021-12-07 11:47:56 +0100

    Set up local server with self-signed certificate

---
+++ server.js
@@ -0,0 +1,26 @@
+const {createServer} = require('https');
+const {parse} = require('url');
+const {readFileSync} = require('fs');
+const next = require('next');
+
+const port = 3000;
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({dev});
+const handle = app.getRequestHandler();
+
+const httpsOptions = {
+    key: readFileSync('./.cert/key.pem'),
+    cert: readFileSync('./.cert/cert.pem'),
+    passphrase: 'asdf'
+};
+
+app.prepare()
+    .then(() => {
+        createServer(httpsOptions, (req, res) => {
+            const parsedUrl = parse(req.url, true);
+            handle(req, res, parsedUrl);
+        }).listen(port, err => {
+            if (err) throw Error(`Server error: ${err}`);
+            console.log(`> Ready on https://localhost:${port}`);
+        })
+    });
