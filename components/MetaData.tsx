b4efbe29ee5a72973b4f7a91e8874aca24758136
Patrik Medveď <patrikm@kentico.com>
2021-12-07 15:31:59 +0100

    Initialize custom element with data from Kontent

---
+++ components/MetaData.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import styles from "./MetaData.module.css";
+
+type Props = {
+  readonly title: string;
+  readonly value: string
+}
+
+export const MetaData: React.FC<Props> = ({
+                                     title,
+                                     value,
+                                   }) => {
+  return (
+    <div className={styles.row}>
+      <h2>{title}</h2>
+      <div>{value}</div>
+    </div>
+  )
+}
