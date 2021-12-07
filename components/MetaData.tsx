7d433589642ccc0c370f306031f8ea1792943963
Patrik Medveď <patrikm@kentico.com>
2021-12-07 15:37:22 +0100

    Set up prettier

--- components/MetaData.tsx
+++ components/MetaData.tsx
@@ -1,19 +1,16 @@
-import React from 'react';
+import React from "react";
 import styles from "./MetaData.module.css";
 
 type Props = {
   readonly title: string;
-  readonly value: string
-}
+  readonly value: string;
+};
 
-export const MetaData: React.FC<Props> = ({
-                                     title,
-                                     value,
-                                   }) => {
+export const MetaData: React.FC<Props> = ({ title, value }) => {
   return (
     <div className={styles.row}>
       <h2>{title}</h2>
       <div>{value}</div>
     </div>
-  )
-}
+  );
+};
